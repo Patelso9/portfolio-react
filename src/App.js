9581c94305bfd7d7180c46fb1a0bd09e3e8cd84b
Princess <sonali.patel.pkg@gmail.com>
2021-07-11 15:07:03 -0500

    fixed a few original bugs

--- src/App.js
+++ src/App.js
@@ -1,14 +1,57 @@
-import React, {useeState, useEffect} from 'react';
-import axios from 'axios;'
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import './App.css';
+import Repo from './components/Repo';
 
 
 function App() {
 
+  // ------ Github Repo Search -------//
+  const [repo, setRepo] = useState([])
+  const [search, setSearch] = useState('')
+
+  useEffect(()=> {
+    axios.get('https://api.github.com/users/Patelso9/repos')
+    .then(res => {
+      setRepo(res.data);
+    })
+    .catch(err => console.log(err))
+  }, [])
+
+  const handleChange = e => {
+    setSearch(e.target.value)
+  }
+
+  const filteredRepo = repo.filter(repos =>
+    repos.name.toLowerCase().includes(search.toLowerCase))
+
+
+  //---------- HTML -----------//
   return (
     <div className="profile-App">
+      <div className="repo-App">
+          <div className="repo-name">
+              <h1 className="repo-text">Search for a repository</h1>
+              <form>
+                <input 
+                  type="text" 
+                  placeholder="search for a repository" 
+                  className="repo-input" 
+                  onChange={handleChange}
+                />
+              </form>
+          </div>
+          {filteredRepo.map(repos => {
+              return <Repo
+                  name={repos.name}
+                  html_url={repos.html_url}
+                  language={repos.language}
+              />
+          })}  
+      </div>
     </div>
-  );
+  )
+
 }
 
 export default App;
--- src/components/Repo.jsx
+++ src/components/Repo.jsx
@@ -1,43 +1,16 @@
-import React from 'react'
-import RepoProj from './RepoProj'
+import React from 'react';
+import './Repo.css'
 
-const Repo = () => {
-
-    const [repo, setRepo] = useState([])
-    const [search, setSearch] = useState('')
-  
-    useEffect(()=> {
-      axios.get('https://api.github.com/users/Patelso9/repos')
-      .them(res => {
-        setRepo(res.data);
-      }).catch(err => console.log(err))
-    }, [])
-  
-    const handleChange = e => {
-      setSearch(e.target.value)
-    }
-  
-    const filteredRepo = repo.filter(repo =>
-      repo.name.includes(search))
-
+const Repo = ({ name, html_url, language }) => {
     return (
-        <div className="repo-App">
-            <div className="repo-name">
-                <h1 className="repo-text">Search for a repository</h1>
-                <form type="text" 
-                    placeholder="search for a repository" 
-                    className="repo-input" 
-                    onChange={handleChange}
-                />
+        <div className="Proj-container">
+            <div className="proj-row">
+                <div className="proj">
+                    <h1 className="proj-name">{name}</h1>
+                    <p className="proj-language">{language}</p>
+                    <p className="proj-url">{html_url}</p>
+                </div>
             </div>
-            {filteredRepo.map(repo => {
-                return <RepoProj
-                    name={repo.name}
-                    name={repo.html_url}
-                    name={repo.language}
-                />
-            })}
-            
         </div>
     )
 }
