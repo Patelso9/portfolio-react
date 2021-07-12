eb0c5b2c1b8d994a493a2e91cf781e919892d558
Princess <sonali.patel.pkg@gmail.com>
2021-07-12 01:26:43 -0500

    ucss update

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './About.css'
 
 const About = () => {
 
@@ -8,7 +9,13 @@ const About = () => {
     return (
         <div>
             <h2>About Me</h2>
-            <p>Welcome to my profile! I am based out of Chicago, IL and pursuing a software development certification from Rutgers University. I have a bachelors in packaging sciences from Michigan State University. My previous jobs included roles dealing with packaging engineering and project management. I most recently worked as a project manager at Diageo where I lead a crossfunctional team into commercialize the 2019 Whiskey of the year (George Dickel Bottled in Bond). Prior to Diageo, I worked at Nest Fragrances as the primary product development engineer where I helped design and develop packaging for all components (candles, perfume, lotion, soap, etc).</p>
+            <p>Welcome to my profile! 
+                <br />I am based out of Chicago, IL and pursuing a software development certification from Rutgers University. 
+                <br />
+                <br />I have a bachelors in packaging sciences from Michigan State University.
+                <br />My previous jobs included roles dealing with packaging engineering and project management. 
+                <br />Most recently, I worked as a project manager at Diageo where I lead a crossfunctional team into commercialize the 2019 Whiskey of the year (George Dickel Bottled in Bond). 
+                <br />Prior to Diageo, I worked at Nest Fragrances as the primary product development engineer where I helped design and develop packaging for all components (candles, perfume, lotion, soap, etc).</p>
             {/* <img ></img> */}
         </div>
     )
